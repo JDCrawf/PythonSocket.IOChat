4eddd6c737923872378e230b4c72a6fce3c5703d
unknown <JDCrawford100@gmail.com>
2024-01-23 12:25:44 -0800

    Working on the GUI layout
    Making progress creating the GUI for the chat app
    Added a dictionary to track widgets created within functions that may need to be updated later
    Tweaking layout of the frames
    There is an 'issue' with the widths and heights of certain widgets.
        Some widgets use pixels, some use character height/widths, the difference is not entire clear since both will use the 'width' and 'height' parameters

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import tkinter as tk
+from tkinter import scrolledtext
+import json
 
+widget = {}
 
 # Initialize the root window
 def init_root():
@@ -64,7 +67,10 @@ def build_server_view(_frame):
 		@param _frame: The root frame of the application.
 		@return: The server view frame for the application.
 	'''
-	return tk.Frame(_frame, width=200, height=350, bd=4, relief=tk.SUNKEN)
+	_server_list = tk.Frame(_frame, width=200, height=350, bd=4, relief=tk.SUNKEN)
+	
+	# 
+	return _server_list
 
 def build_chat_log(_frame):
 	'''
@@ -72,7 +78,21 @@ def build_chat_log(_frame):
 		@param frame: The root frame of the application.
 		@return: The chat log frame for the application.
 	'''
-	return tk.Frame(_frame, width=400, height=450, bd=4, relief=tk.SUNKEN)
+	_chat_frame = tk.Frame(_frame, width=200, height=450)
+
+	# Label to display current server's name
+	server_label = tk.Label(_chat_frame, text="Server Name", width=10, anchor='w')
+	server_label.grid(column=0, row=0, sticky='NEW')
+	widget['server_name'] = server_label
+
+	# Messages sent to/from the server
+	# The state is set to DISABLED to prevent editing on the client side
+	# When the server sends a message, it will need to be set to NORMAL, add the new message, then reset it to DISABLED
+	chat_log= scrolledtext.ScrolledText(_chat_frame, width=50, height=28, state=tk.DISABLED)# height is in characters, not pixels -_-
+	chat_log.grid(column=0, row=1, sticky='SEW')
+	widget['chat_log'] = chat_log
+
+	return _chat_frame
 
 def build_text_entry(_frame):
 	'''
@@ -82,26 +102,50 @@ def build_text_entry(_frame):
 		@param _frame: The root frame of the application.
 		@return: The text entry frame for the application.
 	'''
-	return tk.Frame(_frame, width=400, height=50, bd=4, relief=tk.SUNKEN)
+	_text_entry = tk.Frame(_frame, width=400, height=20, bd=1, relief=tk.SUNKEN)
+	
+	# Label to display client's Username
+	name_box = tk.Label(_text_entry, text="Username", width=10)
+	name_box.grid(column=0, row=0, sticky='NSW')
+	widget['username'] = name_box
+
+	# Entry box to type message to send to server
+	# This should take focus right after connecting to a server
+	entry_box = tk.Entry(_text_entry, width=50)
+	entry_box.grid(column=1, row=0, sticky='NS')
+
+	# Button to send the message from the entry box to the server
+	# Enter key should also send the message to the server
+	send_button = tk.Button(_text_entry, text="Send")
+	send_button.grid(column=2, row=0, sticky='NSE')
+	return _text_entry
 
 def build_window(_root):
+	'''
+		Builds and returns the root window for the application.
+		This will create all the subframes for the application.
+		@param _root: The root window for the application.
+		@return: The root window with the GUI built for the application.
+	'''
 	menu_frame = build_menu(_root)
 	_root.config(menu=menu_frame)
 	
 	# Create a container frame to hold the profile and the server view frames
-	info_frame = tk.Frame(_root, width=200, height=500, bd=4, relief=tk.SUNKEN)
+	info_frame = tk.Frame(_root, width=200, bd=2, relief=tk.SUNKEN)
 	info_frame.grid(row=0, column=0,sticky='NSW')
 		
 	# create a container frame to hold the chat menu and the text entry frames
-	chat_frame = tk.Frame(_root, width=400, height=500, bd=4, relief=tk.SUNKEN)
+	chat_frame = tk.Frame(_root, width=400, bd=2, relief=tk.SUNKEN)
 	chat_frame.grid(row=0, column=1, sticky='NSE')
-		
+	
+	# create a container frame to hold the profile information
 	profile_frame = build_profile(info_frame)
 	profile_frame.grid(row=0, column=0, sticky='NEW')
 		
+	# create a container frame to hold the server information
 	server_frame = build_server_view(info_frame)
 	server_frame.grid(row=1, column=0, sticky='SEW')
-
+	
 	chat_log_frame = build_chat_log(chat_frame)
 	chat_log_frame.grid(row=0, column=0, sticky='NEW')
 
@@ -114,6 +158,7 @@ def build_window(_root):
 root = init_root()
 build_window(root)
 
+print(widget)
 # main function
 def main():
 	root.mainloop()
